42e068bd070c59e6ff044832e99dc1798bda4dfc
Михаил <mishashapka@icloud.com>
2022-10-14 18:05:26 +0300

    message

--- bot1.py
+++ bot1.py
@@ -3,3 +3,4 @@ cmd.run("cd C:/Users/ShapkaMY/Desktop/github/date", check=True, shell=True)
 cmd.run('git add .', check=True, shell=True)
 
 cmd.run('git commit -m "message"', check=True, shell=True)
+cmd.run("git push -u origin master -f", check=True, shell=True)
