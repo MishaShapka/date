8b3d392689420f074f6c4fc0e47ccef83c130f92
Михаил <mishashapka@icloud.com>
2022-10-14 18:05:13 +0300

    message

---
+++ bot1.py
@@ -0,0 +1,5 @@
+import subprocess as cmd
+cmd.run("cd C:/Users/ShapkaMY/Desktop/github/date", check=True, shell=True)
+cmd.run('git add .', check=True, shell=True)
+
+cmd.run('git commit -m "message"', check=True, shell=True)
