e331ae8b9d12a85b89c3cb2331d63f49dc0b3a75
Михаил <mishashapka@icloud.com>
2022-10-14 18:11:01 +0300

    message

--- bot_date_github.py
+++ bot_date_github.py
@@ -1,6 +1,6 @@
 import subprocess as cmd
 
-with open("README.mb", "a") as myfile:
+with open("date.txt", "a") as myfile:
     myfile.write("appended text")
 
 cmd.run("cd C:/Users/ShapkaMY/Desktop/github/date", check=True, shell=True)
