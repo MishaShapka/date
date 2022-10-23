9a543a24172c9976f77d3679747251aa5ea4ebaf
Михаил <mishashapka@icloud.com>
2022-10-23 13:39:05 +0300

    :boom:

--- bot_date_github.py
+++ bot_date_github.py
@@ -10,5 +10,3 @@ cmd.run("cd C:/Users/ShapkaMY/Desktop/github/date", check=True, shell=True)
 cmd.run('git add .', check=True, shell=True)
 cmd.run('git commit -m "message"', check=True, shell=True)
 cmd.run("git push origin", check=True, shell=True)
-
-os.system("pause")
